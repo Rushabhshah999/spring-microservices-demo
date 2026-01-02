704232493894d01844042b3c8a5b484cfa334718
Rushabh Shah <shah.rushabh.shah@gmail.com>
2026-01-02 18:17:28 +0100

    comment added

--- service-b/src/main/java/com/company/ServiceAClient.java
+++ service-b/src/main/java/com/company/ServiceAClient.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
+
+//name = "service-a" must match  spring.application.name of the provider.
+//No url is needed.
+
 @FeignClient(name = "service-a" ,  configuration = ServiceAFeignConfig.class) // Notice: no URL!
 public interface ServiceAClient {
 
